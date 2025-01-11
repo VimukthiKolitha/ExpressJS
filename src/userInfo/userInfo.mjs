e27ac9ed9eb5831b9b5b867822254805149fcc61
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-11 21:44:22 +0530

    user data

---
+++ src/userInfo/userInfo.mjs
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id:1,
+        name:'vimukthi'
+    },
+    {
+        id:2,
+        name:'kolitha'
+    },
+    {
+        id:3,
+        name:'jaywardena'
+    }
+]
