d2225c7f13ccd8de7f37caaa0b3838dcacb0765b
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-11 21:45:36 +0530

    product data

---
+++ src/userInfo/productInfo.mjs
@@ -0,0 +1,14 @@
+export  const productInfo = [
+    {
+      id:1,
+      name:'soap'
+    },
+    {
+        id:2,
+        name:'brush'
+    },
+    {
+        id:3,
+        name:'broom'
+    },
+]
