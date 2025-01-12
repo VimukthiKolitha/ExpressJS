ea004fe1dea79f45322f8b593d981a396e0490bb
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-12 18:46:58 +0530

    crud

--- src/Routers/user.mjs
+++ src/Routers/user.mjs
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { data } from "../userInfo/userInfo.mjs";
 
+import { PrismaClient } from '@prisma/client';
+const DB = new PrismaClient();
+
+
 const userRouter = Router();
 
 //get all users
@@ -20,6 +24,7 @@ userRouter.get('/api/v1/user/UserID',(req,res) =>{
     if(id!== undefined && id !== '')
     {
         //user id comes as string i this poit we convert that string to integer number
+        //ex:http://localhost:4000/api/v2/useInfo/productID?id = 1
         const user = data.find(u => u.id === Number(id));
 
         return  res.status(200).json({
@@ -33,6 +38,30 @@ userRouter.get('/api/v1/user/UserID',(req,res) =>{
         msg:'id not defene'
      })
 })
+
+//create new user
+/*api post req :
+{
+    "Name":"asela",
+    "UserName":"kolitha",
+    "Password":"123"
+}
+    */
+userRouter.post("/api/v1/user/Create-user",async(req,res)=>{
+    const userdata = req.body
+    console.log(userdata);
+   
+    //user data store in database
+    try {
+        //User means schema.prisma db table name
+        const newUser  = await DB.User.create({data:userdata});
+        return res.status(201).json({newUser});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    }
+});
 //login
 //register
 
