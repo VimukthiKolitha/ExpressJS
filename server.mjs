00130df213f3bf54f3e27fda4a602c2184c71364
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-12 18:44:51 +0530

    main file

--- server.mjs
+++ server.mjs
@@ -5,9 +5,14 @@ import product from './src/Routers/product.mjs';
 //execute express
 const server = express();
 
+//using json midle ware for the catch json data coming from post (create user)
+server.use(express.json());
+
+
 //use routers
 server.use(userRouter);
 server.use(product);
+
 
 server.listen(4000,()=>{
     console.log('server running...');
