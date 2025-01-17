de16c7fe27de128575976d50605df80797fa8669
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-17 21:06:58 +0530

    midlewares

--- src/Routers/midleware.mjs
+++ src/Routers/midleware.mjs
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {log} from "console";
 import testmid from "../utils/midle.mjs"
+import { checkSchema, matchedData, query, validationResult } from "express-validator";
 
 const mid = Router();
 
@@ -12,5 +13,42 @@ mid.get('/api/v2/Router/mid',testmid,
     next();
 },(req,res) =>{
     log('3');
+})
+
+//validate name using 'express-validator'
+//'query("name")' means we mention name in endpoint (ex:http://localhost:4000/abc?name=13)
+//in here i am checking is name email and is age Numeric value
+mid.get('/abc',query("name").isEmail().withMessage('this is not an email'),query('age').isNumeric().withMessage('this is not boolean'),(req,res) =>{
+   const r = validationResult(req);
+   //match data catch
+   const match = matchedData(req);
+
+   console.log(r.array());
+   console.log(match);
+   res.sendStatus(200);
+})
+
+//mech schema method
+//ex:http://localhost:4000/efg?age=we
+mid.get('/efg',checkSchema({
+    name:{
+        //check is name email
+        isEmail:{
+          errorMessage:'not an email'
+        }
+    },
+    age:{
+        //check is age number
+        isNumeric:{
+            errorMessage:'not a number'
+        }
+    }
+}),(req,res) =>{
+    const r = validationResult(req);
+    const d  = matchedData(req);
+
+    console.log(r.array());
+    console.log(d);
+
 })
 export default mid;
